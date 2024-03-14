7c5b429971e5357c298c143de78bf1e7840433d7
josuepolania <32796807-josuepolania@users.noreply.replit.com>
2024-03-14 02:26:55 +0000

    Initial commit

---
+++ tipos_de_datos.py
@@ -0,0 +1,23 @@
+#string
+nombre = "josue"
+nombre = 'josue'
+print('nombre =>', nombre)
+
+print(type(nombre))
+
+# int
+edad = 30
+print('edad =>', edad)
+print(type(edad))
+
+# boolean
+is_single = True
+print('is_single =>', is_single)
+print(type(is_single))
+
+
+# inputs
+nombre = input("cual es tu edad?")
+print("usando input", nombre)
+print(type(nombre))
+
---
+++ variables.py
@@ -0,0 +1,23 @@
+print("variables")
+
+# Esto es un avariable nombre
+nombre = "Juan"
+print(nombre)
+
+# Esto es una variable con un numero
+edad = 30
+print(edad)
+
+nombre = "david"
+print(nombre)
+
+#input
+
+nombre = input("¿Cual es tu nombre?")
+print("usando input", nombre)
+
+edad = input("¿Cual es tu edad?")
+print("usando input", edad)
+
+
+      
